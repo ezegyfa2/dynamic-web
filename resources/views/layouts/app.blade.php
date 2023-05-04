75c20cb1eacef44f945296cbf112a8c7b6adea13
Darvas Istvan <darvasvok@gmail.com>
2023-05-04 23:32:04 +0300

    Adding ip meta tag to layout view

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width,initial-scale=1.0">
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
+    <meta name="ip" content="{{ Request::ip() }}">
 
     <title>{{ config('app.name', 'Laravel') }}</title>
 
