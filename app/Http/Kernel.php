bd3818ca6841dc724df23c50de5e25dcee3b70c3
ezegyfa2 <darvasvok@gmail.com>
2023-10-05 05:25:04 +0300

    Adding compression

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -37,6 +37,7 @@ class Kernel extends HttpKernel
             \App\Http\Middleware\ShareErrorsFromSession::class,
             \App\Http\Middleware\VerifyCsrfToken::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
+            \ErlandMuchasaj\LaravelGzip\Middleware\GzipEncodeResponse::class,
         ],
 
         'api' => [
