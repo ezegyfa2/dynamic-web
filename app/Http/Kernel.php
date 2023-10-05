0396eb3e813028a170c9ea09bef17d16e4cd9c24
ezegyfa2 <darvasvok@gmail.com>
2023-10-05 20:50:39 +0300

    Fixing https

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -38,6 +38,7 @@ class Kernel extends HttpKernel
             \App\Http\Middleware\VerifyCsrfToken::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
             \ErlandMuchasaj\LaravelGzip\Middleware\GzipEncodeResponse::class,
+            \Ezegyfa\LaravelHelperMethods\HttpsRedirecter::class,
         ],
 
         'api' => [
@@ -65,6 +66,5 @@ class Kernel extends HttpKernel
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
         'setLanguage' => \Ezegyfa\LaravelHelperMethods\Language\Middleware::class,
-        'https' => \Ezegyfa\LaravelHelperMethods\HttpsRedirecter::class,
     ];
 }
