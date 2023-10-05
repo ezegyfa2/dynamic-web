b045918bc6ca9b9d534d8fd22c9ff4d5e89ca1df
ezegyfa2 <darvasvok@gmail.com>
2023-10-05 05:25:36 +0300

    Production

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,6 +1,8 @@
-const mix = require('laravel-mix');
+const mix = require('laravel-mix')
+require('laravel-mix-compress')
 require('helper-vue-components/AssetCompiler.js').default
 
 //compileAssets(mix, 'dynamic-web-vue-components', [ 'Welcome' ])
 //compileAssets(mix, 'dynamic-web-vue-components', [ 'RequestOffer' ])
 compileAssets(mix, 'dynamic-web-vue-components', [ 'ThankYou' ])
+mix.compress()
