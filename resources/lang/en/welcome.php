6fe8d373d23a53dc0c36f9a53b2e807c10951fca
József Bagota <jozsef.bagota@gmail.com>
2023-10-12 15:51:32 +0200

    translations

--- resources/lang/en/welcome.php
+++ resources/lang/en/welcome.php
@@ -57,14 +57,17 @@ return [
     'article1' => [
         'title' => 'Improve your credibility',
         'content' => ' In business, it is not uncommon to have several companies in the market that offer a similar product or service to yours.  In order to stand out from your competitors, credibility is vital. Because customers expect trustworthy businesses to have a website and social media accounts, you need an attractive, professional website to help you build a strong online presence and communicate quality information to consumers. ',
+        'image_alt' => 'Improve your credibility',
     ],
     'article2' => [
         'title' => '24/7 Contact',
         'content' => ' In business, it is important to always be available to your customers. One of the biggest advantages of a website is that anyone, anywhere, anytime can use it conveniently because it is always available. This means that your customers can use the services and access the information they need about your business outside of working hours and in their free time. ',
+        'image_alt' => '24/7 Contact',
     ],
     'article3' => [
         'title' => 'Go online',
         'content' => ' In addition to being quickly accessible, a website allows you to get your product or service anywhere in the world. However, the biggest challenge you face is getting your website found in this busy online world. A website provides your prospective customers with a simple, one-stop platform where they can get to know you and your business, increasing your visibility and sales. ',
+        'image_alt' => 'Go online',
     ],
     'article_button_content' => 'Request offer',
     'workflow' => [
@@ -73,18 +76,22 @@ return [
         'sketch' => [
             'title' => 'Outline',
             'content' => 'We agree with the client all the details of the website and then gather the necessary materials, images, texts',
+            'image_alt' => 'Outline',
         ],
         'design' => [
             'title' => 'Design',
             'content' => 'Based on the outline, we design the websites functions and appearance',
+            'image_alt' => 'Design',
         ],
         'develop' => [
             'title' => 'Development',
             'content' => 'Based on the design, we will create the most suitable website for your company using the latest web development techniques',
+            'image_alt' => 'Development',
         ],
         'test' => [
             'title' => 'Testing',
             'content' => 'We use testers to check the websites functionality and ensure that it works correctly',
+            'image_alt' => 'Testing',
         ],
     ],
     'team' => [
@@ -92,24 +99,28 @@ return [
         'members' => [
             'darvas_istvan' => [
                 'name' => 'I am a backend and frontend developer with almost 5 years of experience. I am involved in the development of the websites look and functionality',
+                'image_alt' => 'István Darvas&amp;apos;s picture',
                 'twitter_aria_label' => 'István Darvas&amp;apos;s twitter page',
                 'facebook_aria_label' => 'István Darvas&amp;apos;s facebook page',
                 'linkedin_aria_label' => 'István Darvas&amp;apos;s linkedin page'
             ],
             'bagota_jozsef' => [
                 'name' => 'I have been designing website layouts and images for 8 years using the latest design technologies and methods',
+                'image_alt' => 'József Bagota&amp;apos;s picture',
                 'twitter_aria_label' => 'József Bagota&amp;apos;s twitter page',
                 'facebook_aria_label' => 'József Bagota&amp;apos;s facebook page',
                 'linkedin_aria_label' => 'József Bagota&amp;apos;s linkedin page'
             ],
             'benedek_csaba' => [
                 'name' => 'I work as a Javascript developer, with more than 2 years of experience in developing client and server side functionality',
+                'image_alt' => 'Csaba Benedek&amp;apos;s picture',
                 'twitter_aria_label' => 'Csaba Benedek&amp;apos;s twitter page',
                 'facebook_aria_label' => 'Csaba Benedek&amp;apos;s facebook page',
                 'linkedin_aria_label' => 'Csaba Benedek&amp;apos;s linkedin page'
             ],
             'kovacs_szilard' => [
                 'name' => 'I work in frontend development, developing the look and feel of websites. I have almost 2 years of experience',
+                'image_alt' => 'Szilárd Kovács&amp;apos;s picture',
                 'twitter_aria_label' => 'Szilárd Kovács&amp;apos;s twitter page',
                 'facebook_aria_label' => 'Szilárd Kovács&amp;apos;s facebook page',
                 'linkedin_aria_label' => 'Szilárd Kovács&amp;apos;s linkedin page'
@@ -118,6 +129,14 @@ return [
     ],
     'our_works' => [
         'title' => 'Our works',
+        'image_alt' => 'website 1',
+        'image_alt' => 'website 2',
+        'image_alt' => 'website 3',
+        'image_alt' => 'website 4',
+        'image_alt' => 'website 5',
+        'image_alt' => 'website 6',
+        'image_alt' => 'website 7',
+        'image_alt' => 'website 8',
     ],
     'request_offer' => [
         'title' => 'Request offer',
