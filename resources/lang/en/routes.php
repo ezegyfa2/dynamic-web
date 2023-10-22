5c54953903b56359805db7ef2ae8f8ab74fb4a6b
ezegyfa2 <darvasvok@gmail.com>
2023-10-23 01:39:43 +0300

    Fixing

--- resources/lang/en/routes.php
+++ resources/lang/en/routes.php
@@ -3,5 +3,6 @@
 return [
     'request-offer' => 'request-offer',
     'thank-you' => 'thank-you',
-    'privacy-policy' => 'privacy-policy'
+    'privacy-policy' => 'privacy-policy',
+    'contact' => 'contact'
 ];
