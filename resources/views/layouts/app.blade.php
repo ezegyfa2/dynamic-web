7e73d876b2b0451b6e73da8dd138d42e83ff743e
ezegyfa2 <darvasvok@gmail.com>
2023-10-22 22:27:33 +0300

    Remove tagmanager

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,15 +4,6 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width,initial-scale=1.0">
     <meta name="google-site-verification" content="JK4EX9_9uTT7dpTGpBl4V4FlRMp3oCFXw5VCQxSoUo0" />
-    @if (app()->environment('production'))
-        <!-- Google Tag Manager -->
-        <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-        })(window,document,'script','dataLayer','GTM-N4NBSVF');</script>
-        <!-- End Google Tag Manager -->
-    @endif
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <meta name="ip" content="{{ Request::ip() }}">
@@ -45,12 +36,5 @@
             @yield('content')
         </main>
     </div>
-
-    @if (app()->environment('production'))
-        <!-- Google Tag Manager (noscript) -->
-        <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-N4NBSVF"
-        height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
-        <!-- End Google Tag Manager (noscript) -->
-    @endif
 </body>
 </html>
