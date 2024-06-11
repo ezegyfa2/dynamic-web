e621a4d623d06748c27bfe6ead8e9756f913a948
ezegyfa2 <darvasvok@gmail.com>
2024-06-11 12:50:15 +0300

    Fixing

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -14,10 +14,17 @@ use stdClass;
 class HomeController extends Controller
 {
     public function welcome() {
+        $templateParams = DynamicTemplateMethods::getTemplateLayoutParams();
+        $templateParams->contact_form = new \stdClass;
+        $templateParams->contact_form->form_item_sections = json_decode(file_get_contents(base_path('app/Templates/contactUsTableInfos.json')));
+        foreach ($templateParams->contact_form->form_item_sections as $formItemSection) {
+            $formItemSection->data->label = __($formItemSection->data->label);
+            $formItemSection->data->placeholder = __($formItemSection->data->placeholder);
+        }
         return DynamicTemplateMethods::getTranslatedTemplateDynamicPage(
             'dynamic_web_welcome', 
             'welcome', 
-            $this->getTemplateLayoutParams(),
+            $templateParams,
             [ 
                 'welcome',
             ],
@@ -161,6 +168,45 @@ class HomeController extends Controller
                 'thankYou',
             ]
         );
+    }
+
+    public function storeContactMessage(Request $request) {
+        try {
+            $validationRules = [
+                "name" => [
+                    "string",
+                    "max:255",
+                    "required",
+                ],
+                "email" => [
+                    "string",
+                    "max:255",
+                    "required",
+                    "email",
+                ],
+                "phone" => [
+                    "string",
+                    "max:15",
+                    "required",
+                    "regex:/([0-9]|\+){0,14}/"
+                ],
+                "message" => "required",
+            ];
+            $request->validate($validationRules);
+            $requestData = request()->all();
+            $insertData = [
+                'name' => $requestData['name'],
+                'email' => $requestData['email'],
+                'phone' => $requestData['phone'],
+                'message' => $requestData['message'],
+            ];
+            \DB::table('contact_messages')->insert($insertData);
+        }
+        catch (ValidationException $e) {
+            $errorMessages = HttpMethods::updateErrors($e->errors(), $e->validator->failed());
+            return redirect()->back()->withInput(request()->all())->withErrors($errorMessages);
+        }
+        return redirect(__('routes.thank-you'))->with('success_message', __('welcome.contact_success'));
     }
 
     public function getTemplateLayoutParams() {
