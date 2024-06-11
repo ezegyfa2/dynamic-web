cdf607c61246022c07d5826ce17d2aa17ff4052a
ezegyfa2 <darvasvok@gmail.com>
2024-06-11 06:45:20 +0300

    Fixing webpack.mix

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,6 +1,48 @@
-const mix = require('laravel-mix');
-require('helper-vue-components/AssetCompiler.js').default
+const mix = require('laravel-mix')
+const path = require('path')
+const { size } = require('lodash')
+require('laravel-mix-compress')
+require('helper-vue-components/AssetCompiler.js')
+const headerBackgroundImageGenerator = require('dynamic-web-vue-components/Welcome/Header/Header/images/generator.js')
+const articleImageGenerator = require('dynamic-web-vue-components/Welcome/Articles/Description/images/generator.js')
+const latestWorkImageGenerator = require('dynamic-web-vue-components/Welcome/LatestWorks/images/generator.js')
 
-//compileAssets(mix, 'dynamic-web-vue-components', [ 'Welcome' ])
+global.currentWebpackConfig = {
+    module: {
+        rules: [
+            {
+                test: /\.pug$/,
+                loader: 'pug-plain-loader'
+            },
+            {
+                test: /\.scss$/,
+                use: [
+                    'sass-loader'
+                ]
+            }
+        ]
+    },
+    output: {
+        chunkFilename: 'js/[name].bundle.js',
+        publicPath: '/',
+    }
+}
+
+mix.webpackConfig(currentWebpackConfig)
+//mix.js('resources/js/basicPackages.js', 'public/js').vue()
+//mix.js('resources/js/welcome.js', 'public/js').vue()
+//mix.js('resources/js/thankYou.js', 'public/js').vue()
+//mix.sass('resources/sass/welcome.scss', 'public/css/')
+/*mix.copy(
+    path.join('node_modules', 'dynamic-web-vue-components', 'src', 'Welcome', 'CompiledTemplate.json'), 
+    path.join('app', 'Templates', 'Welcome.json')
+)*/
+compileAssets(mix, 'dynamic-web-vue-components', [ 'Welcome' ])
 //compileAssets(mix, 'dynamic-web-vue-components', [ 'RequestOffer' ])
-compileAssets(mix, 'dynamic-web-vue-components', [ 'ThankYou' ])
+//compileAssets(mix, 'dynamic-web-vue-components', [ 'ThankYou' ])
+//replaceComponentRegistrations('./node_modules/helper-vue-components/src')
+
+//headerBackgroundImageGenerator.generate()
+//articleImageGenerator.generate()
+//latestWorkImageGenerator.generate()
+//replaceImageCache('app/Http/ImageCache.php')
